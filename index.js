bd3a3085abfd8f97a6c35f364dd2f9ac6b62530c
ozan192 <ozan59222@gmail.com>
2025-02-20 22:31:32 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions, MessageManager, Embed, Collection } = require(`discord.js`);
+const fs = require('fs');
+const client = new Client({ intents: 32767 }); 
+const configfile = require('./config.json');
+
+client.commands = new Collection();
+
+const functions = fs.readdirSync("./functions").filter(file => file.endsWith(".js"));
+const eventFiles = fs.readdirSync("./events").filter(file => file.endsWith(".js"));
+const commandFolders = fs.readdirSync("./commands");
+
+process.on('warning', (warning) => {
+    
+});
+
+(async () => {
+    for (file of functions) {
+        require(`./functions/${file}`)(client);
+    }
+    client.handleEvents(eventFiles, "./events");
+    client.handleCommands(commandFolders, "./commands");
+    client.login(configfile["token"])
+})();
+
